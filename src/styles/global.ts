d69e29e83faf5e9b958a3aab56f2e4fcafab6113
David <daviddev1306@gmail.com>
2021-11-10 12:31:03 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle `
+    :root {
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --green: #33CC95;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --background: #f0f2f5;
+        --shape: #ffffff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    // font-size: 16px (ideal para desktop)
+    // REM = 1REM = 16px
+    html {
+        @media (max-width: 1080px) {
+            font-size: 87.75%; // isso equivale a 15px
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%; // equivale a 14px
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400; // geralmente o padrao da fonte poppins é 400
+    }
+
+    h1, h2, h3, h4, h5, h6, strong { //sāo as tags de negrito
+        font-weight: 600;
+
+    }
+
+    button {
+        cursor: pointer; // é botão clicável
+    }
+
+    [disabled] {
+        opacity: 0.6; // tudo que estiver disabilitado ficará mais claro
+        cursor: not-allowed; //mostra icone de não permitido
+    }
+`
