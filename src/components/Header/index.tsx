881ccd69031eb1ab2d4c26e36daf61526414437e
David <daviddev1306@gmail.com>
2021-11-23 10:56:57 -0300

    third commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
-import logoImg from '../../assets/Logo.svg'
-import { Container, Content } from './styles'
 
-export function Header() {
+import logoImg from '../../assets/Logo.svg';
+import { Container, Content } from './styles';
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+
+export function Header({onOpenNewTransactionModal }: HeaderProps) {
+   
+   
     return (
         <Container>
            <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+            <button type="button" onClick= {onOpenNewTransactionModal}>
                 Nova transação
             </button>
-            </Content>
+           </Content>
         </Container>
     )
 }
